73de7b70526f74b429d9b72ffca68362edba077a
Anmol Aafre <anmol.aafre27@gmail.com>
2025-07-21 14:45:42 +0530

    Update messenger.py

--- messenger.py
+++ messenger.py
@@ -108,5 +108,5 @@ def webhook():
         return "EVENT_RECEIVED", 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8000)
